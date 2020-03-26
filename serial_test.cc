019630862ca8c00a01e3d6ec57b2a3ee5c611e30
Sergey A. Baranov <s.baranov@vniias.ru>
2020-03-26 18:04:37 +0300

    CHANGE:add_type_datas

--- serial_test.cc
+++ serial_test.cc
@@ -198,20 +198,20 @@ next_iteration:
                 tums1_conn_ref.msgBuffer.push_back (static_cast<uint8_t>(read_c));
                 if (read_c == ')')
                 {
-                    if (tums1_conn_ref.msgBuffer.size () == 31)
+                    if (tums1_conn_ref.msgBuffer.size () == 32)
                     {
                         printf (" <+NEW_MSG>");
                         
                         // Run crc16 routine
                         //BufferIterator = msgBuffer.begin ();
                         //BufferIterator++;
-                        uiCrc16Calc = CalculateCRC16 (&tums1_conn_ref.msgBuffer [1], 25);
+                        uiCrc16Calc = CalculateCRC16 (&tums1_conn_ref.msgBuffer [1], 26);
                         printf (" <CRC16 = 0x%04X>", uiCrc16Calc);
                         uiCrc16Recv = 0x0000;
-                        uiCrc16Recv  =  RoutineCRC16Char2i16 (tums1_conn_ref.msgBuffer [29]);
-                        uiCrc16Recv |= (RoutineCRC16Char2i16 (tums1_conn_ref.msgBuffer [28]) << 4);
-                        uiCrc16Recv |= (RoutineCRC16Char2i16 (tums1_conn_ref.msgBuffer [27]) << 8);
-                        uiCrc16Recv |= (RoutineCRC16Char2i16 (tums1_conn_ref.msgBuffer [26]) << 12);
+                        uiCrc16Recv  =  RoutineCRC16Char2i16 (tums1_conn_ref.msgBuffer [30]);
+                        uiCrc16Recv |= (RoutineCRC16Char2i16 (tums1_conn_ref.msgBuffer [29]) << 4);
+                        uiCrc16Recv |= (RoutineCRC16Char2i16 (tums1_conn_ref.msgBuffer [28]) << 8);
+                        uiCrc16Recv |= (RoutineCRC16Char2i16 (tums1_conn_ref.msgBuffer [27]) << 12);
                         if (uiCrc16Calc == uiCrc16Recv)
                             printf (" <+CRC16>");
                         else;
