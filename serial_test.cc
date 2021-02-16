37d482fe5db7163c9361627e27cb20c17952cd95
Sergey A. Baranov <baranovsa@mail.ru>
2021-02-16 19:53:55 +0300

    CHANGE:bad_crc16_processing

--- serial_test.cc
+++ serial_test.cc
@@ -1,6 +1,8 @@
 //comment
 //  HELP for brain
 //       https://ru.stackoverflow.com/questions/376364/%D0%A7%D1%82%D0%BE-%D0%B4%D0%B5%D0%BB%D0%B0%D0%B5%D1%82-select-%D0%B8-fd-isset
+//       https://crccalc 
+//               (CRC-16/GENIBUS)
 
 // system include
 #include <stdio.h>
@@ -32,6 +34,7 @@ typedef struct
     uint8_t _StationID [6];
     uint8_t _MsgNum [4];
     uint8_t _DataHead;
+    uint8_t _TUMSNum;
     uint8_t _DataGroup;
     uint8_t _DataSubGroup;
     uint8_t _Data [5];
@@ -58,6 +61,7 @@ typedef struct
     uint8_t _StationID [6];
     uint8_t _MsgNum [4];
     uint8_t _DataHead;
+    uint8_t _TUMSNum;
     uint8_t _DataGroup;
     uint8_t _DataSubGroup;
     uint8_t _CRC16 [4];
@@ -316,7 +320,7 @@ int main (int argc, char** argv)
             tums1_conn_ref.msgBuffer.push_back (static_cast<uint8_t>(d));
             if (d == ')')
             {
-                if (tums1_conn_ref.msgBuffer.size () == sizeof (TTUMSIn)) // 32 
+                if (tums1_conn_ref.msgBuffer.size () == sizeof (TTUMSIn)) // 33
                 {
                     tums1_conn_ref.msgBuffer.push_back (0x00); // only for print to screen !!!
                     printf ("\033[1;33m%s\033[0m", (char*) &tums1_conn_ref.msgBuffer [0]);
@@ -325,44 +329,48 @@ int main (int argc, char** argv)
                     tums1_conn_ref.msg_in = (TTUMSIn*) &tums1_conn_ref.msgBuffer [0];
                     // Run crc16 routine
                     // Skeep '(', ')', CRC16 field
-                    uiCrc16Calc = CalculateCRC16 (&tums1_conn_ref.msg_in->_StationID [0], sizeof (TTUMSIn)-1-4-1); //26
+                    uiCrc16Calc = CalculateCRC16 (&tums1_conn_ref.msg_in->_StationID [0], sizeof (TTUMSIn)-1-4-1); //27
                     //printf (" <CRC16(calc) = 0x%04X>", uiCrc16Calc);
                     uiCrc16Recv = 0x0000;
                     uiCrc16Recv  =  RoutineCRC16Char2ui16 (tums1_conn_ref.msg_in->_CRC16 [3]);
                     uiCrc16Recv |= (RoutineCRC16Char2ui16 (tums1_conn_ref.msg_in->_CRC16 [2]) << 4);
                     uiCrc16Recv |= (RoutineCRC16Char2ui16 (tums1_conn_ref.msg_in->_CRC16 [1]) << 8);
                     uiCrc16Recv |= (RoutineCRC16Char2ui16 (tums1_conn_ref.msg_in->_CRC16 [0]) << 12);
-                    if (uiCrc16Calc == uiCrc16Recv)
-                          printf ("\033[1;32m<+CRC16>\033[0m");
-                    else  printf ("\033[1;31m<+CRC16>\033[0m");
+                    if (uiCrc16Calc != uiCrc16Recv)
+                          printf ("\033[1;31m<-CRC16(0x%04X)>\033[0m", uiCrc16Calc);
+                    else 
+                    { 
+                        printf ("\033[1;32m<+CRC16>\033[0m");
 
-                    // send answer
-                    bzero (&tums1_conn_ref.msg_out, sizeof (TTUMSOut));
-                    tums1_conn_ref.msg_out._BracketStart  = '(';
-                    tums1_conn_ref.msg_out._StationID [0] = tums1_conn_ref.msg_in->_StationID [0];
-                    tums1_conn_ref.msg_out._StationID [1] = tums1_conn_ref.msg_in->_StationID [1];
-                    tums1_conn_ref.msg_out._StationID [2] = tums1_conn_ref.msg_in->_StationID [2];
-                    tums1_conn_ref.msg_out._StationID [3] = tums1_conn_ref.msg_in->_StationID [3];
-                    tums1_conn_ref.msg_out._StationID [4] = tums1_conn_ref.msg_in->_StationID [4];
-                    tums1_conn_ref.msg_out._StationID [5] = tums1_conn_ref.msg_in->_StationID [5];
-                    tums1_conn_ref.msg_out._MsgNum [0]    = tums1_conn_ref.msg_in->_MsgNum [0];
-                    tums1_conn_ref.msg_out._MsgNum [1]    = tums1_conn_ref.msg_in->_MsgNum [1];
-                    tums1_conn_ref.msg_out._MsgNum [2]    = tums1_conn_ref.msg_in->_MsgNum [2];
-                    tums1_conn_ref.msg_out._MsgNum [3]    = tums1_conn_ref.msg_in->_MsgNum [3];
-                    tums1_conn_ref.msg_out._DataHead      = 'A'; //msg_in->_DataHead;
-                    tums1_conn_ref.msg_out._DataGroup     = tums1_conn_ref.msg_in->_DataGroup;
-                    tums1_conn_ref.msg_out._DataSubGroup  = tums1_conn_ref.msg_in->_DataSubGroup;
-                    uiCrc16Calc = CalculateCRC16 (&tums1_conn_ref.msg_out._StationID [0], 13); //sizeof (TTUMSOut)-2-4); //13
-                    tums1_conn_ref.msg_out._CRC16 [3]     =  RoutineCRC16ui162Char ( uiCrc16Calc & 0x000F);
-                    tums1_conn_ref.msg_out._CRC16 [2]     =  RoutineCRC16ui162Char ((uiCrc16Calc & 0x00F0) >> 4);
-                    tums1_conn_ref.msg_out._CRC16 [1]     =  RoutineCRC16ui162Char ((uiCrc16Calc & 0x0F00) >> 8);
-                    tums1_conn_ref.msg_out._CRC16 [0]     =  RoutineCRC16ui162Char ((uiCrc16Calc & 0xF000) >> 12);
-                    tums1_conn_ref.msg_out._BracketEnd    = ')';
-                    tums1_conn_ref.sp_write.Write ((char*) &tums1_conn_ref.msg_out, sizeof (TTUMSOut));
-                    tums1_conn_ref.sp_write.Write (&_ps, 1);
-                    tums1_conn_ref.sp_write.Write (&_vk, 1);
+                        // send answer
+                        bzero (&tums1_conn_ref.msg_out, sizeof (TTUMSOut));
+                        tums1_conn_ref.msg_out._BracketStart  = '(';
+                        tums1_conn_ref.msg_out._StationID [0] = tums1_conn_ref.msg_in->_StationID [0];
+                        tums1_conn_ref.msg_out._StationID [1] = tums1_conn_ref.msg_in->_StationID [1];
+                        tums1_conn_ref.msg_out._StationID [2] = tums1_conn_ref.msg_in->_StationID [2];
+                        tums1_conn_ref.msg_out._StationID [3] = tums1_conn_ref.msg_in->_StationID [3];
+                        tums1_conn_ref.msg_out._StationID [4] = tums1_conn_ref.msg_in->_StationID [4];
+                        tums1_conn_ref.msg_out._StationID [5] = tums1_conn_ref.msg_in->_StationID [5];
+                        tums1_conn_ref.msg_out._MsgNum [0]    = tums1_conn_ref.msg_in->_MsgNum [0];
+                        tums1_conn_ref.msg_out._MsgNum [1]    = tums1_conn_ref.msg_in->_MsgNum [1];
+                        tums1_conn_ref.msg_out._MsgNum [2]    = tums1_conn_ref.msg_in->_MsgNum [2];
+                        tums1_conn_ref.msg_out._MsgNum [3]    = tums1_conn_ref.msg_in->_MsgNum [3];
+                        tums1_conn_ref.msg_out._DataHead      = 'A'; //msg_in->_DataHead;
+                        tums1_conn_ref.msg_out._TUMSNum       = tums1_conn_ref.msg_in->_TUMSNum;
+                        tums1_conn_ref.msg_out._DataGroup     = tums1_conn_ref.msg_in->_DataGroup;
+                        tums1_conn_ref.msg_out._DataSubGroup  = tums1_conn_ref.msg_in->_DataSubGroup;
+                        uiCrc16Calc = CalculateCRC16 (&tums1_conn_ref.msg_out._StationID [0], 14); //sizeof (TTUMSOut)-2-4); //13
+                        tums1_conn_ref.msg_out._CRC16 [3]     =  RoutineCRC16ui162Char ( uiCrc16Calc & 0x000F);
+                        tums1_conn_ref.msg_out._CRC16 [2]     =  RoutineCRC16ui162Char ((uiCrc16Calc & 0x00F0) >> 4);
+                        tums1_conn_ref.msg_out._CRC16 [1]     =  RoutineCRC16ui162Char ((uiCrc16Calc & 0x0F00) >> 8);
+                        tums1_conn_ref.msg_out._CRC16 [0]     =  RoutineCRC16ui162Char ((uiCrc16Calc & 0xF000) >> 12);
+                        tums1_conn_ref.msg_out._BracketEnd    = ')';
+                        tums1_conn_ref.sp_write.Write ((char*) &tums1_conn_ref.msg_out, sizeof (TTUMSOut));
+                        tums1_conn_ref.sp_write.Write (&_ps, 1);
+                        tums1_conn_ref.sp_write.Write (&_vk, 1);
 
-                    printf ("\033[1;35m%.19s\033[0m", (char*) &tums1_conn_ref.msg_out);
+                        printf ("\033[1;35m%.20s\033[0m", (char*) &tums1_conn_ref.msg_out);
+                    }
                 }
                 else
                 {
